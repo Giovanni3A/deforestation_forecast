bef5ad29d9182279b016867a142fdbff552cf5d0
Giovanni Amorim <giovanni.amorim@elogroup.com.br>
2023-09-10 21:35:45 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+# data folder
+DATA_PATH = r"C:\Users\GiovanniAmorim\Pessoal\Mestrado\DL2CV\data"
+
+# data layers
+RAW_DATA_PATH = os.path.join(DATA_PATH, "raw")
+TRUSTED_DATA_PATH = os.path.join(DATA_PATH, "trusted")
+
+# raw data
+AMAZON_FRONTIER_DATA = os.path.join(RAW_DATA_PATH, "brazilian_legal_amazon")
+INITIAL_DEFORESTATION = os.path.join(RAW_DATA_PATH, "accumulated_deforestation_2007")
+DETER_DATA = os.path.join(RAW_DATA_PATH, "deter-amz-public-2023set08")
+PRODES_DATA = os.path.join(RAW_DATA_PATH, "yearly_deforestation")
+
+# trusted data
+TR_DEFORESTATION = os.path.join(TRUSTED_DATA_PATH, "deforestation.pkl")
+TR_FRAMES = os.path.join(TRUSTED_DATA_PATH, "frames_detail")
+
+# temporal limits
+DT_INIT = "2016-08-01"
+DT_FIM = "2022-10-01"
