dc6af4e9e3e570020cadaf97a3bab4827a005e3c
Giovanni Amorim <giovanni.amorim@elogroup.com.br>
2023-10-22 21:59:07 -0300

    precipitation input

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ INITIAL_DEFORESTATION = os.path.join(RAW_DATA_PATH, "accumulated_deforestation_2
 DETER_DATA = os.path.join(RAW_DATA_PATH, "deter-amz-public-2023set08")
 PRODES_DATA = os.path.join(RAW_DATA_PATH, "yearly_deforestation")
 COUNTIES_DATA = os.path.join(RAW_DATA_PATH, "municipios")
+RAIN_DATA = os.path.join(RAW_DATA_PATH, "precipitations")
 
 # trusted data
 TR_DEFORESTATION = os.path.join(TRUSTED_DATA_PATH, "deforestation.csv")
@@ -21,6 +22,7 @@ TR_FRAMES = os.path.join(TRUSTED_DATA_PATH, "frames_detail")
 TR_FRAMES_IDX = os.path.join(TRUSTED_DATA_PATH, "frames_idx.csv")
 TR_COUNTIES = os.path.join(TRUSTED_DATA_PATH, "counties.csv")
 TR_COUNTIES_DEFOR = os.path.join(TRUSTED_DATA_PATH, "counties_defor.csv")
+TR_RAIN_AVG = os.path.join(TRUSTED_DATA_PATH, "avg_precipitation.csv")
 
 # temporal limits
 DT_INIT = "2016-07-01"
